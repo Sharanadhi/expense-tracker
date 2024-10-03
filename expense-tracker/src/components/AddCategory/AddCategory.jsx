5063d18416a898bd62a528cff693ce1ba413f4cb
Sharan Athiyadath <sharanadhi@gmail.com>
2024-10-03 16:39:06 -0300

    added form submit functionality

--- expense-tracker/src/components/AddCategory/AddCategory.jsx
+++ expense-tracker/src/components/AddCategory/AddCategory.jsx
@@ -1,9 +1,20 @@
+import axios from 'axios'
 import './AddCategory.scss';
+
 export default function AddCategory() {
+  const baseUrl='http://localhost:8080/';
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const category_name= e.target.categoryInput.value;
+    const response = await axios.post(`${baseUrl}addCategory`,{category_name});
+    e.target.reset();
+    alert(response.data.message);
+  }
+
   return (
     <section className="add-category">
       <h1 className="add-category__page-title">ADD CATEGORY</h1>
-      <form className="add-category__form">
+      <form className="add-category__form" onSubmit={handleSubmit}>
         <div className="add-category__wrapper">
           <label htmlFor="categoryInput" className="add-category__label">
             Category
